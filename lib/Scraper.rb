035443af555db5603c2713c7285cbfc036a840c6
hudbuz <hzb2@cornell.edu>
2016-03-24 11:34:25 -0400

    Added validation in post title to accept Free Agents and Players

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -22,8 +22,8 @@ class Rotoworld::Scraper
       post = Rotoworld::Post.new
 
       post_title_info = player.css("div.headline div.player").text
-      player_name = player.css("div.headline div.player a").text
-      player_position_team = post_title_info.match(/- .+\s+-+\s+.+[st$]/)
+      player_name = player.css("div.headline div.player a").first.text
+      player_position_team = post_title_info.match(/- .+\s+-+\s+.+[str$]/)
       post.title = "#{player_name}#{player_position_team}"
       post.headline = player.css("div.report p").text
       post.impact = player.css("div.impact").first.text.strip
